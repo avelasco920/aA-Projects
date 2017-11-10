0d2593a9bee78bed7c75dda9f46982d6c073f837
Angel Velasco <avelasco920@gmail.com>
2017-11-09 21:19:19 -0800

    developed DOMNodeCollection class

--- W6D4 - jQuery Lite/lib/jquery_lite.js
+++ W6D4 - jQuery Lite/lib/jquery_lite.js
@@ -71,7 +71,10 @@ const DOMNodeCollection = __webpack_require__ (1)
 
 window.$l = (selector) => {
   if (typeof selector === 'string') {
-    return document.querySelectorAll(selector);
+    let nodes = Array.from(document.querySelectorAll(selector));
+    return new DOMNodeCollection(nodes);
+  } else if (selector instanceof HTMLElement) {
+    return new DOMNodeCollection([selector]);
   }
 }
 
@@ -82,6 +85,77 @@ window.$l = (selector) => {
 /***/ (function(module, exports) {
 
 class DOMNodeCollection {
+  constructor(nodes) {
+    this.nodes = nodes;
+  }
+  
+  html(arg) {
+    if (arg) {
+      this.nodes.forEach(function(node) {
+        node.innerHTML = arg;
+      });
+    } else {
+      return this.nodes[0].innerHTML;
+    }
+  }
+  
+  empty() {
+    this.html("");
+  }
+  
+  append(argument) {
+    if (argument instanceof HTMLElement) {
+      this.nodes.forEach(function(node) {
+        node.appendChild(argument);
+      });
+    } else if (typeof argument === 'string') {
+      this.nodes.forEach(function(node) {
+        node.innerHTML += argument;
+      });
+    } else {
+      this.nodes.forEach(function(node) {
+        argument.nodes.forEach(function(elToAppend) {
+          node.append(elToAppend.cloneNode(true));
+        });
+      }); 
+    }
+  }
+  
+  children() {
+    let allChildren = [];
+    this.nodes.forEach(function(node) {
+      allChildren = allChildren.concat(node.children);
+    });
+    return new DOMNodeCollection(allChildren);
+  }
+  
+  parent() {
+    let allParents = [];
+    this.nodes.forEach(function({parentNode}){
+      debugger;
+      if (!parentNode.visited) {
+        parentNode.visited = true;
+        allParents = allParents.concat(parentNode);
+      }
+    });
+    allParents.forEach(function(node){
+      node.visited = null;
+    })
+    return new DOMNodeCollection(allParents);
+  }
+  
+  find(selector) {
+    if (typeof selector === 'string') {
+      let nodes = Array.from(document.querySelectorAll(selector));
+      return new DOMNodeCollection(nodes);
+    } else if (selector instanceof HTMLElement) {
+      return new DOMNodeCollection([selector]);
+    }
+  }
+  
+  remove(selector) {
+    this.each(node => node.parentNode.removeChild(node));
+  }
   
 }
 
--- W6D4 - jQuery Lite/lib/main.js
+++ W6D4 - jQuery Lite/lib/main.js
@@ -2,7 +2,10 @@ const DOMNodeCollection = require ('./dom_node_collection.js')
 
 window.$l = (selector) => {
   if (typeof selector === 'string') {
-    return document.querySelectorAll(selector);
+    let nodes = Array.from(document.querySelectorAll(selector));
+    return new DOMNodeCollection(nodes);
+  } else if (selector instanceof HTMLElement) {
+    return new DOMNodeCollection([selector]);
   }
 }
 
